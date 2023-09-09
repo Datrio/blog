4deb6021f67070f8cdf6b313962c34e9ed994d83
Dariusz Siedlecki <datrio@gmail.com>
2023-09-09 12:21:53 +0200

    forgot scripts

--- assets/js/ai_popup.js
+++ assets/js/ai_popup.js
@@ -1,3 +1,10 @@
-$(() => {
-  console.log("hello")
+document.addEventListener("DOMContentLoaded", () => {
+  new bootstrap.Popover(document.querySelector('[data-bs-toggle="popover"]'), {
+    container: 'body',
+    placement: 'bottom',
+    trigger: 'focus hover',
+    html: true,
+    content: document.querySelector('div.tldr > ul'),
+    fallbackPlacements: ['bottom']
+  })
 })
